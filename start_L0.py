94c8da3ccee16f7465d9ca9e577a647d1df0bba0
gisilves <gianluigi.silvestre@gmail.com>
2024-10-26 15:05:52 +0200

    Added start/stop python scripts (WIP)

---
+++ start_L0.py
@@ -0,0 +1,61 @@
+import sys
+import datetime
+import time
+from L0 import (
+    clear_autotrigger, clear_circ_buffer, read_trig_status, read_buff_status,
+    stop_eventpoll, set_busy_off, execute_command, log_last_file
+)
+
+# Initialize variables
+cal_num = 0
+dat_num = 0
+
+# Read the values from runnum.conf
+with open("runnum.conf", "r") as f:
+    for line in f:
+        if line.startswith("cal_num"):
+            cal_num = int(line.split()[2])
+        elif line.startswith("dat_num"):
+            dat_num = int(line.split()[2])
+
+# Define paths and start polling commands
+start_cal_polling = f"my-t W JMDC-SELF 1F0600 {cal_num:02x} 0C"
+start_dat_polling = f"my-t W JMDC-SELF 1F0600 {dat_num:02x} 0D"
+pathL0 = "/Data/BLOCKS/USBLF_PCGSC03/"
+logfile = open('log.txt', 'a')
+
+def send_command_and_log(timestamp=0, run_type=0):
+    unixTime = int(time.time())
+    logfile.write(f"{unixTime}: starting run\n")
+    
+    execute_command(clear_autotrigger)
+    execute_command(clear_circ_buffer)
+    execute_command(read_trig_status)
+    execute_command(read_buff_status)
+    execute_command(start_cal_polling if run_type == 0 else start_dat_polling)
+    # Update cal_num and dat_num
+    if run_type == 0:
+        cal_num += 1
+        logfile.write(f"{unixTime}: starting CAL number {cal_num:02x}\n")
+        logfile.write(f"{unixTime}: {start_cal_polling}\n")
+    else:
+        logfile.write(f"{unixTime}: starting DAT number {dat_num:02x}\n")
+        logfile.write(f"{unixTime}: {start_dat_polling}\n")
+        dat_num += 1
+
+    # Update runnum.conf
+    with open("runnum.conf", "w") as f:
+        f.write("cal_num = " + str(cal_num) + "\n")
+        f.write("dat_num = " + str(dat_num) + "\n")
+    logfile.write(f"{unixTime}: {start_cal_polling if run_type == 0 else start_dat_polling}\n")
+    execute_command(set_busy_off)
+    execute_command(read_trig_status)
+    log_last_file(logfile, unixTime, pathL0)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: python start_L0.py <run_type (0 CAL; 1 DAT)>")
+        sys.exit(0)
+    else:
+        run_type = int(sys.argv[1])
+        send_command_and_log(0, run_type)
---
+++ stop_L0.py
@@ -0,0 +1,25 @@
+import datetime
+import time
+from L0 import (
+    set_busy_on, read_trig_status, read_buff_status, stop_eventpoll, 
+    execute_command, log_last_file
+)
+
+pathL0 = "/Data/BLOCKS/USBLF_PCGSC03/"
+logfile = open('log.txt', 'a')
+
+def send_command_and_log(timestamp=0):
+    unixTime = int(time.time())
+    logfile.write(f"{unixTime}: stopping run\n")
+
+    execute_command(set_busy_on)
+    execute_command(read_trig_status)
+    execute_command(read_buff_status)
+    execute_command(stop_eventpoll)
+    logfile.write(f"{unixTime}: stopping eventpoll\n")
+    logfile.write(f"{unixTime}: {stop_eventpoll}\n")
+    log_last_file(logfile, unixTime, pathL0)
+    time.sleep(60)
+
+if __name__ == '__main__':
+    send_command_and_log(0)

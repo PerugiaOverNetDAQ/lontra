dcf863b84c648d29740bb24d76c55c13a61b0e32
gisilves <gianluigi.silvestre@gmail.com>
2024-10-27 17:58:54 +0100

    Updated server test script with message from SPS24 BT

--- server_test.py
+++ server_test.py
@@ -1,94 +1,69 @@
-#!/usr/bin/env python
-
-## server_herd.py
-# Author: L. Di Venere
-# This script should run on the PC controlling the DAQ of each subdetector
-# The script waits for the message from the client and executes the start or stop functions depending on the message content
-# If the execution of these function is successful, the server sends back the message to the client.
-#
-# NOTE: change the IP address in the script to the current IP address of the PC running this script (server).
-# NOTE: check the firewall configuration of the server pc, to be able to send/receive TCP/IP message over a specific port .
-
 import socket
 import sys
-import os
-import subprocess
 import datetime
-import time
-
-
-def send_command_and_log(cmd=0, run_number=0, timestamp=0, run_type=0):
-    print("received run_number %i" % run_number, file=sys.stderr)
-    print("received run_type %i" % run_type, file=sys.stderr)
-    print("received cmd %i" % cmd, file=sys.stderr)
-    print("received timestamp %i" % timestamp, file=sys.stderr)
-
-    run_type_s = "UNDEF"
-    if run_type == 0:
-        run_type_s = "CAL"
-    elif run_type == 1:
-        run_type_s = "BEAM"
-
-    presentDate = datetime.datetime.now()
-    unixTime = time.mktime(presentDate.timetuple())
-    # print regular python date&time
-    #    print("date_time =>", presentDate)
-    # displaying unix timestamp after conversion
-    print("unix_timestamp => %d" % unixTime)
-
-    #    pippo = os.system("echo prova")
-    #    pippo = subprocess.run(["echo", "prova"], capture_output=True)
-
-    if cmd == 1:  # START
-        print("%d: starting run" % unixTime)
-        print("%d: run number = %d" % (unixTime, run_number))
-        print("%d: timestamp from server = %d" % (unixTime, timestamp))
-        print("%d: run_type = %d (%s)" % (unixTime, run_type, run_type_s))
-    elif cmd == 0:  # STOP
-        print("%d: stopping run" % unixTime)
-
-    return 0
-
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 # Bind the socket to the port
-# server_address = ('127.0.0.1', 10000) ## CHANGE HERE THE IP ADDRESS OF THE SERVER PC.
-server_address = ("", 8888)  ## CHANGE HERE THE IP ADDRESS OF THE SERVER PC.
+server_address = ('192.168.0.60', 8888)  # CHANGE HERE THE IP ADDRESS OF THE SERVER PC.
 print("starting up on %s port %s" % server_address, file=sys.stderr)
 sock.bind(server_address)
-
 # Listen for incoming connections
 sock.listen(1)
 
+listen_address = '192.168.0.105'
+
 while True:
-    # Wait for a connection
     print("waiting for a connection", file=sys.stderr)
     connection, client_address = sock.accept()
+    
+    if client_address[0] != listen_address:
+        print("connection from", client_address, file=sys.stderr)
+        print("client not allowed, closing connection", file=sys.stderr)
+        connection.close()
+        continue
+
     try:
         print("connection from", client_address)
 
-        # Receive the data in small chunks and retransmit it
+        # Buffer to accumulate data until the newline character
+        data_buffer = b''
+
+        # Loop to receive data until a newline character is detected
         while True:
-            data = connection.recv(16)
-            # print ('received "%s"' % data, file=sys.stderr)
+            data = connection.recv(1024)
             if data:
-                # Decode data
-                run_number = int.from_bytes(data[4:6], "big")
-                run_type = int.from_bytes(data[6:8], "big")
-                cmd = int.from_bytes(data[11:12], "big")
-                timestamp = int.from_bytes(data[12:16], "big")
-                print("received run_number %i" % run_number, file=sys.stderr)
-                print("received run_type %i" % run_type, file=sys.stderr)
-                print("received cmd %i" % cmd, file=sys.stderr)
-                print("received timestamp %i" % timestamp, file=sys.stderr)
-                send_command_and_log(cmd, run_number, timestamp, run_type)
-                # print (sys.stderr, 'sending data back to the client')
-                connection.sendall(data)
+                data_buffer += data  # Accumulate data chunks
+                if b'\n' in data_buffer:
+                    # Process the complete message
+                    complete_message = data_buffer.strip().decode()  # Remove newline and decode
+                    print(f'received complete message: "{complete_message}"', file=sys.stderr)
+                    
+                    # Process the message in the form YYYYMMDD_HHMM_trigType_cmd
+                    # Example: 20231027_1740_0_0
+                    # trigType: 0 = CAL, 1 = BEAM
+                    # cmd: 0 = start, 1 = stop
+
+                    date = complete_message[0:8]
+                    time = complete_message[8:12]
+                    trigType = complete_message[13]
+                    cmd = complete_message[15]
+
+                    # Convert date and time to unix timestamp
+                    date_obj = datetime.datetime.strptime(date, '%Y%m%d')
+                    time_obj = datetime.datetime.strptime(time, '%H%M')
+                    unix_timestamp = int(datetime.datetime.timestamp(date_obj) + datetime.timedelta(hours=time_obj.hour, minutes=time_obj.minute).total_seconds())
+                    print(f'unix timestamp: {unix_timestamp}', file=sys.stderr)
+
+                    print(f'trigType: {trigType}', file=sys.stderr)
+                    print(f'cmd: {cmd}', file=sys.stderr)
+
+                    # Clear the buffer after processing
+                    data_buffer = b''
+                    break  # Exit after processing a single complete message
             else:
                 print("no more data from", client_address, file=sys.stderr)
                 break
     finally:
-        # Clean up the connection
         connection.close()
